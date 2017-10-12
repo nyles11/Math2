f6bbb69697b0f5b1b6fa13b826726c53b4204b15
Rob Hardy <robhardy777@gmail.com>
2017-10-11 22:30:18 -0400

    2nd commit

---
+++ src/Math2.java
@@ -0,0 +1,12 @@
+public class Math2 {
+    public static void main(String[] args) {
+        int mynumber;
+        double myothernumber;
+        mynumber = 2;
+        myothernumber = 1.7938;
+
+        System.out.println("My favorite number, " + mynumber + " plus my other favorite number, " + myothernumber + ", is 3.7938");
+
+
+    }
+}
